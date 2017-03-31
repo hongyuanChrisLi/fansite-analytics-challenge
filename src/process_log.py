d4ce4dc3f88f448292c2e117f829a2ec9d0caad0
Chris Li <kittokevin@gmail.com>
2017-03-31 13:04:56 -0700

    OOP

--- src/process_log.py
+++ src/process_log.py
@@ -1,18 +1,48 @@
 from pyspark import SparkConf, SparkContext
 
-conf = SparkConf().setMaster('local').setAppName('Insight')
-sc = SparkContext(conf=conf)
+
+class ProcessLog:
+    def __init__(self):
+        conf = SparkConf().setMaster('local').setAppName('Insight')
+        self.sc = SparkContext(conf=conf)
+        self.input_rdd = self.sc.textFile('../log_input/log.txt')
+
+    def find_top_hosts(self):
+        pair_rdd = self.input_rdd.map(ProcessLog.__host_map__)
+        err_rdd = pair_rdd.filter(lambda x: x[1] == 0)
+        res = pair_rdd.reduceByKey(lambda x, y: x + y) \
+            .top(10, key=lambda x: x[1])
+        print(err_rdd.count())
+        print (res)
+
+    @staticmethod
+    def __print_rdd__(x):
+        print (x)
+
+    @staticmethod
+    def __host_map__(x):
+        num = 0
+        items = x.split('- -')
+        if len(items) == 2:
+            # check if the record is valid
+            num = 1
+
+        host = items[0].rstrip()
+        return host, num
 
 
-def print_rdd(x):
-    print (x)
 
-input_rdd = sc.textFile('../log_input/log.txt')
-split_rdd = input_rdd.map(lambda line: line.split('- -'))
-pair_rdd = split_rdd.map(lambda x: (x[0].rstrip(), 1))
-res = pair_rdd.reduceByKey(lambda x, y: x + y)\
-    .top(10, key=lambda x: x[1])
-print (res)
+pl = ProcessLog()
+pl.find_top_hosts()
+
+
+
+# print (input_rdd.getNumPartitions())
+# split_rdd = input_rdd.map(lambda line: line.split('- -'))
+# pair_rdd = split_rdd.map(lambda x: (x[0].rstrip(), 1))
+# res = pair_rdd.reduceByKey(lambda x, y: x + y)\
+#     .top(10, key=lambda x: x[1])
+# print (res)
 # .foreach(print_rdd)
 
 # pair_rdd =
