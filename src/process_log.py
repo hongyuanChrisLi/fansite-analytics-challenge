0bcdb03c616d7ca5ac419d9d38aaa3b227c8ef92
Chris Li <kittokevin@gmail.com>
2017-03-31 10:39:37 -0700

    Feature 1: count top 10 most active host/IP

--- src/process_log.py
+++ src/process_log.py
@@ -4,4 +4,15 @@ conf = SparkConf().setMaster('local').setAppName('Insight')
 sc = SparkContext(conf=conf)
 
 
-sc.textFile('../log_input/log.txt')
+def print_rdd(x):
+    print (x)
+
+input_rdd = sc.textFile('../log_input/log.txt')
+split_rdd = input_rdd.map(lambda line: line.split('- -'))
+pair_rdd = split_rdd.map(lambda x: (x[0].rstrip(), 1))
+res = pair_rdd.reduceByKey(lambda x, y: x + y)\
+    .top(10, key=lambda x: x[1])
+print (res)
+# .foreach(print_rdd)
+
+# pair_rdd =
