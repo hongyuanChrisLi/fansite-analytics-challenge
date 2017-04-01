587a3ca45e74440ec7a92cb1ad6763e0243709d6
Chris Li <kittokevin@gmail.com>
2017-04-01 10:44:17 -0700

    Sat commit

--- src/process_log.py
+++ src/process_log.py
@@ -1,5 +1,7 @@
 from pyspark import SparkConf, SparkContext
 
+import re
+
 
 class ProcessLog:
     def __init__(self):
@@ -15,34 +17,79 @@ class ProcessLog:
         print(err_rdd.count())
         print (res)
 
+    def find_top_resource(self):
+        pair_rdd = self.input_rdd.map(ProcessLog.__resource_map__)
+        # valid_rdd = pair_rdd.filter(lambda x: x[0])
+        res = pair_rdd.reduceByKey(lambda x, y: x + y) \
+            .top(10, key=lambda x: x[1])
+
+        print (res)
+
+
     @staticmethod
     def __print_rdd__(x):
         print (x)
 
     @staticmethod
     def __host_map__(x):
-        num = 0
+        count = 0
         items = x.split('- -')
         if len(items) == 2:
             # check if the record is valid
-            num = 1
+            count = 1
 
         host = items[0].rstrip()
-        return host, num
+        return host, count
 
+    @staticmethod
+    def __resource_map__(x):
+        resource = ''
+        reply_bytes = 0
+
+        str_splits = x.split(']')
+
+        if len(str_splits) == 2:
+            second_str = str_splits[1]
+            # e.g. "GET /history/apollo/ HTTP/1.0" 200 6245
+
+            req_lst = re.findall('"([^"]*)"', second_str)
+            # e.g. GET /history/apollo/ HTTP/1.0
+
+            if len(req_lst) == 1:
+                req_str = req_lst[0]
+                # req_splits = req_str.split()
+
+                resource = ProcessLog.__get_resource__(req_str)
+
+                if resource:
+                    reply_bytes = ProcessLog.__get_reply_bytes__(second_str.replace('"' + req_str + '"', '').strip())
+
+        return resource, reply_bytes
+
+    @staticmethod
+    def __get_resource__(req):
+
+        resource = ''
+
+        if req.startswith('GET'):
+            resource = req[3:]
+        elif req.startswith(('POST', 'HEAD')):
+            resource = req[4:]
+        resource = resource.replace('HTTP/1.0', '').strip()
+        return resource
+
+    @staticmethod
+    def __get_reply_bytes__(num_str):
+
+        reply_bytes = 0
+
+        num_lst = num_str.split()
+
+        if (len(num_lst) == 2) and (num_lst[1].isdigit()):
+            reply_bytes = int(num_lst[1])
+
+        return reply_bytes
 
 
 pl = ProcessLog()
-pl.find_top_hosts()
-
-
-
-# print (input_rdd.getNumPartitions())
-# split_rdd = input_rdd.map(lambda line: line.split('- -'))
-# pair_rdd = split_rdd.map(lambda x: (x[0].rstrip(), 1))
-# res = pair_rdd.reduceByKey(lambda x, y: x + y)\
-#     .top(10, key=lambda x: x[1])
-# print (res)
-# .foreach(print_rdd)
-
-# pair_rdd =
+pl.find_top_resource()
